c478f9d4c4bf064d32fd7aa15d35f541cd462303
ahmed.bouzir <ahmed.bouzir@proxym-it.com>
2025-10-24 12:09:18 +0100

    chore: init project

---
+++ src/main/java/com/_ach/backend/TestController.java
@@ -0,0 +1,14 @@
+package com._ach.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+@GetMapping("hello")
+    public String hello() {
+    return "Hello World";
+}
+
+}
